9e885674cb7e6ed6918f7aab87c9315fc26b9acb
hwangyoungjin <yoho555@icloud.com>
2021-08-31 15:14:54 +0900

    Add : PostApiController

--- src/main/kotlin/com/example/mvc/model/request/UserRequest.kt
+++ src/main/kotlin/com/example/mvc/model/request/UserRequest.kt
@@ -1,8 +1,13 @@
 package com.example.mvc.model.request
 
+import com.fasterxml.jackson.annotation.JsonProperty
+
 data class UserRequest ( //data클래스 생성자에서 변수 생성
         var name:String?=null,
         var age:Int?=null,
         var email:String?=null,
-        var address:String?=null
+        var address:String?=null,
+
+        @JsonProperty("phone_number")
+        var phoneNumber:String?=null
 )
